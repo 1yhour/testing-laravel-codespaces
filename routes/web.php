4f9912bbc0b9d5f6d8e8e08bfb271bdadb2facd5
Lyhour Seng <lyhourlucky77@gmail.com>
2026-04-09 13:03:26 +0700

    feat: add default url redirect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+Route::redirect('/','/tasks');
 Route::get("tasks", [TaskController::class,"index"])->name("task.index");
 Route::post("tasks/storeTask", [TaskController::class, "storeTask"])->name("task.storeTask");
 Route::delete("tasks/{id}", [TaskController::class, "deleteTask"])->name("task.deleteTask");
